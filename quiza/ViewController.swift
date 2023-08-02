e2db5be45997f09e8b19101cdb6789759b857a37
Kaan Kalaycioglu <kaanklu@icloud.com>
2023-08-02 23:52:12 +0300

    jjjgjhg

---
+++ quiza/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  quiza
+//
+//  Created by Kaan Kalaycıoğlu on 26.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        asdasdasdas()
+    }
+    
+    func asdasdasdas() {
+        print("fdsfsdsfdfdsfsdfdsfsdfsddsf")
+    }
+
+
+}
+
